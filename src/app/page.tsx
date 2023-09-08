a462a3cfd79b55cbe46178684eb7682c19f98dec
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-08 15:37:15 +0800

    feat: add settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <h1 className="font-display text-5xl">Hello!</h1>
+      <h1 className="font-display text-5xl">Hello!!!</h1>
     </main>
   );
 }
