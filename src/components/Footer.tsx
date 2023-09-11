2cf35075c7c60a4fa23565c4017a87e60c189a9f
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-11 10:36:38 +0800

    feat: style footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,23 @@
 import { createClient } from "@/prismicio";
 import { PrismicLink } from "@prismicio/react";
 import Link from "next/link";
+import Logo from "@/components/Logo";
+import OuterBound from "./OuterBound";
 
 export default async function Footer() {
   const client = createClient();
   const settings = await client.getSingle("settings");
 
   return (
-    <footer>
-      <Link href="/">{settings.data.site_title}</Link>
-      <p>
-        © {new Date().getFullYear()} {settings.data.site_title}
-      </p>
-    </footer>
+    <OuterBound as="footer">
+      <div className="flex sm:flex-row flex-col justify-between items-center gap-6">
+        <Link href="/">
+          <Logo width="120" height="28" />
+        </Link>
+        <p className="text-xs">
+          © {new Date().getFullYear()} {settings.data.site_title}
+        </p>
+      </div>
+    </OuterBound>
   );
 }
