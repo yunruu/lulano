eaeb8e6b158fb4ba33a762c7e5bf5871fd1b9d1c
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-11 10:36:45 +0800

    feat: style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,31 @@
 import { createClient } from "@/prismicio";
 import { PrismicLink } from "@prismicio/react";
 import Link from "next/link";
+import OuterBound from "@/components/OuterBound";
+import Logo from "@/components/Logo";
 
 export default async function Header() {
   const client = createClient();
   const settings = await client.getSingle("settings");
 
   return (
-    <header>
-      <Link href="/">{settings.data.site_title}</Link>
-      <nav>
-        <ul>
-          {settings.data.navigation.map(({ link, label }) => (
-            <li key={label}>
-              <PrismicLink field={link}>{label}</PrismicLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <OuterBound as="header" className="py-4 md:py-6 lg:py-8">
+      <div className="flex gap-4 items-center justify-between sm:flex-row flex-col">
+        <Link href="/">
+          <Logo />
+        </Link>
+        <nav>
+          <ul className="flex">
+            {settings.data.navigation.map(({ link, label }) => (
+              <li key={label}>
+                <PrismicLink field={link} className="p-3">
+                  {label}
+                </PrismicLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </OuterBound>
   );
 }
